dd1cb61704ba1270fde2589d5c5e2b96c2178642
Alexander Frosell <sachafrosell@gmail.com>
2018-10-10 02:03:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,31 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
+  @@genres = []
+  @@artists = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre 
+    @@count += 1 
+    @@genres.push(genre)
+    @@artists.push(artist)
   end 
   
   @@count = 0 
+  
+  def count
+    @@count 
+  end 
+  
+  def genres 
+    @@genres.uniq
+  end 
+  
+  def artists 
+    @@artists.uniq
+  end 
   
   
 end 
